dc66ab9120ec334cd335bcd52d722e88e12b0657
oksirets <39863750+oksirets@users.noreply.github.com>
2018-06-02 22:14:22 +0300

    added project ninja

---
+++ API2/resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Resource.rc
+//
+#define IDR_MENU1                       105
+#define IDI_ICON1                       107
+#define IDC_CURSOR1                     108
+#define IDC_CURSOR2                     109
+#define IDD_DIALOG1                     112
+#define IDB_BITMAP1                     114
+#define IDD_DIALOG2                     115
+#define IDB_BITMAP2                     122
+#define IDR_MENU2                       125
+#define IDD_DIALOG3                     126
+#define IDC_BUTTON1                     1004
+#define IDC_BUTTON2                     1005
+#define ID_QUIT                         40001
+#define ID_INFO_AUTOR                   40002
+#define ID_INFO_ABOUTPROGRAM            40003
+#define ID_TASK                         40004
+#define ID_GAME                         40005
+#define ID_40006                        40006
+#define ID_40007                        40007
+#define ID_40008                        40008
+#define ID_RED                          40009
+#define ID_GREEN                        40010
+#define ID_BLACK                        40011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        128
+#define _APS_NEXT_COMMAND_VALUE         40012
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
